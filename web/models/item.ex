acac85080b505ee5002fa3bb48d9b6b1375bee32
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2016-07-08 17:51:46 +1000

    Messes with the encoder for items to include svg

--- web/models/item.ex
+++ web/models/item.ex
@@ -2,11 +2,11 @@ defmodule UploadTest.Item do
   use UploadTest.Web, :model
   use Arc.Ecto.Schema
 
-  @derive {Poison.Encoder, only: [:name]}
   schema "items" do
     field :name, :string
     field :resource, UploadTest.Resource.Type
     field :vector, UploadTest.Vector.Type
+    field :vector_path, :string, virtual: true
 
     belongs_to :category, UploadTest.Category
 
@@ -22,5 +22,21 @@ defmodule UploadTest.Item do
     |> cast_attachments(params, [:resource])
     |> cast_attachments(params, [:vector])
     |> cast_assoc(:category, required: true)
+  end
+
+  def vector_path(item) do
+    %HTTPoison.Response{body: body} = HTTPoison.get!(UploadTest.Vector.url({item.vector, item}, :original, signed: true))
+    body
+  end
+
+  def encode_item(item) do
+    %UploadTest.Item{item | vector_path: UploadTest.Item.vector_path(item) }
+  end
+
+  defimpl Poison.Encoder, for: UploadTest.Item do
+    def encode(item, options) do
+      encoded_item = UploadTest.Item.encode_item(item)
+      Poison.Encoder.Map.encode(Map.take(encoded_item, [:id, :name, :vector_path]), options)
+    end
   end
 end
