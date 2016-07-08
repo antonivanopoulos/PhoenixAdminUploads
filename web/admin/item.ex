f015c7c2a001d9dd28469f162d25e1d66273cadf
Anton Ivanopoulos <a.ivanopoulos@gmail.com>
2016-07-08 12:03:59 +1000

    Integrates the resource uploader into item admin

--- web/admin/item.ex
+++ web/admin/item.ex
@@ -2,6 +2,22 @@ defmodule UploadTest.ExAdmin.Item do
   use ExAdmin.Register
 
   register_resource UploadTest.Item do
+    show question do
+      attributes_table do
+        row :name
+        row :category
+        row :resource, [], fn(i) ->
+          "<img src=\"#{UploadTest.Resource.url({i.resource, i}, :original, signed: true)}\" />"
+        end
+      end
+    end
 
+    form item do
+      inputs do
+        input item, :name
+        #input item, :category
+        input item, :resource
+      end
+    end
   end
 end
